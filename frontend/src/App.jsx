2a9313362821c8890d86f2da9ff588296ea783e7
Arya Shiju <aryashiju251@gmail.com>
2026-03-10 21:13:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,66 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Login from "./pages/Login.jsx";
-import CitizenDashboard from "./pages/citizen/CitizenDashboard";
-import AuthorityDashboard from "./pages/authority/AuthorityDashboard.jsx";
-import AdminDashboard from "./pages/admin/AdminDashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-// Citizen Pages
-import Notifications from "./pages/citizen/Notifications";
-import Public from "./pages/citizen/Public";
-import ReportIssue from "./pages/citizen/ReportIssue";
-import MyReports from "./pages/citizen/MyReports";
+// Admin
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import SystemAnalytics from "./pages/admin/SystemAnalytics";
+import CategoryManagement from "./pages/admin/CategoryManagement";
+import AuthorityMapping from "./pages/admin/AuthorityMapping";
+import ManageRoles from "./pages/admin/ManageRoles";
+
+// Authority
+import AuthorityDashboard from "./pages/authority/AuthorityDashboard";
+import AssignedIssues from "./pages/authority/AssignedIssues";
+import IssueDetail from "./pages/authority/IssueDetail";
+
+// Public
+import PublicIssueDetail from "./pages/PublicIssueDetail";
+
+// Login
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public route */}
+          <Route path="/issue/:id" element={<PublicIssueDetail />} />
 
-        <Route path="/" element={<Login />} />
+          {/* Login */}
+          <Route path="/" element={<Login />} />
 
-        <Route
-          path="/citizen/*"
-          element={
-            <ProtectedRoute allowedRole="citizen">
-              <CitizenDashboard />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<ReportIssue />} />
-          <Route path="my-reports" element={<MyReports />} />
-          <Route path="notifications" element={<Notifications />} />
-          <Route path="public" element={<Public />} />
-        </Route>
+          {/* Authority routes */}
+          <Route
+            path="/authority"
+            element={
+              <ProtectedRoute allowedRole="authority">
+                <AuthorityDashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<AssignedIssues />} />
+            <Route path="issue/:id" element={<IssueDetail />} />
+          </Route>
 
-        <Route
-          path="/authority/*"
-          element={
-            <ProtectedRoute allowedRole="authority">
-              <AuthorityDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/admin/*"
-          element={
-            <ProtectedRoute allowedRole="admin">
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-      </Routes>
-    </BrowserRouter>
+          {/* Admin routes */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute allowedRole="admin">
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<SystemAnalytics />} />
+            <Route path="categories" element={<CategoryManagement />} />
+            <Route path="mapping" element={<AuthorityMapping />} />
+            <Route path="roles" element={<ManageRoles />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
