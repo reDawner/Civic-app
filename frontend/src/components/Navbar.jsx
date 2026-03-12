a3a7e79be35c27d572366b1d16b6e5b231fc897c
reDawner <145202122+reDawner@users.noreply.github.com>
2026-03-13 02:20:35 +0530

    deleting some files

--- frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar() {
-    return (
-        <div style={{ height: "60px", backgroundColor: "#1e293b", color: "red", display: "flex", alignItems: "center", padding: "0 20px" }}>
-            <h3>Civic Issue App</h3>
-        </div>
-    );
-}
-
-export default Navbar;
